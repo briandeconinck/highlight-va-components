e6fad085d7ee254f1bccace14f560c29dd61d469
Brian DeConinck <brian@briandeconinck.com>
2023-11-01 12:59:20 -0500

    Create bookmarklet.js

---
+++ bookmarklet.js
@@ -0,0 +1,54 @@
+(function () {
+
+    var components = [ 'va-accordion',
+                        'va-additional-info',
+                        'va-alert',
+                        'va-back-to-top',
+                        'va-banner',
+                        'va-official-gov-banner',
+                        'va-promo-banner',
+                        'va-breadcrumbs',
+                        'va-button',
+                        'va-button-pair',
+                        'va-card',
+                        'va-checkbox',
+                        'va-checkbox-group',
+                        'va-crisis-line-modal',
+                        'va-date',
+                        'va-featured-content',
+                        'va-file-input',
+                        'va-link',
+                        'va-loading-indicator',
+                        'va-memorable-date',
+                        'va-minimal-header',
+                        'va-modal',
+                        'va-need-help',
+                        'va-notification',
+                        'va-number-input',
+                        'va-omb-info',
+                        'va-on-this-page',
+                        'va-pagination',
+                        'va-privacy-agreement',
+                        'va-process-list',
+                        'va-progress-bar',
+                        'va-segmented-progress-bar',
+                        'va-radio',
+                        'va-search-input',
+                        'va-select',
+                        'va-sidenav',
+                        'va-table',
+                        'va-telephone',
+                        'va-text-input',
+                        'va-textarea'];
+
+    var styles = document.createElement('style');
+    styles.innerText = '';
+
+    components.forEach((element) => {
+        console.log(element);
+        styles.innerText += element + ' { outline: dashed 0.5rem red; }';
+        styles.innerText += element + ':after { content: "' + element + '"; color: #fff; background-color: red; padding: 0.5rem; }';
+        });
+
+    document.head.appendChild(styles);
+})();
